cf18d85f2bc716afc775942b1eaecb0a994a1e36
unknown <Epulapp@EPULAPP65.univ-lyon1.fr>
2016-05-24 18:02:36 +0200

    valueiteration ongoing

--- TP1_IA_MDP/src/agent/planningagent/ValueIterationAgent.java
+++ TP1_IA_MDP/src/agent/planningagent/ValueIterationAgent.java
@@ -1,14 +1,17 @@
 package agent.planningagent;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 import environnement.Action;
 import environnement.Etat;
 import environnement.MDP;
-import environnement.gridworld.ActionGridworld;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Cet agent met a jour sa fonction de valeur avec value iteration et choisit
@@ -23,17 +26,19 @@ public class ValueIterationAgent extends PlanningValueAgent {
      * discount facteur
      */
     protected double gamma;
-	//*** VOTRE CODE
+    //*** VOTRE CODE
+    protected HashMap<Etat, Double> map;
 
     /**
      *
      * @param gamma
      * @param mdp
+     * @param map
      */
     public ValueIterationAgent(double gamma, MDP mdp) {
         super(mdp);
         this.gamma = gamma;
-		//*** VOTRE CODE
+        //*** VOTRE CODE
 
     }
 
@@ -48,13 +53,30 @@ public class ValueIterationAgent extends PlanningValueAgent {
      */
     @Override
     public void updateV() {
-		//delta est utilise pour detecter la convergence de l'algorithme
+        //delta est utilise pour detecter la convergence de l'algorithme
         //lorsque l'on planifie jusqu'a convergence, on arrete les iterations lorsque
         //delta < epsilon 
         this.delta = 0.0;
-		//*** VOTRE CODE
-                
-		// mise a jour vmax et vmin pour affichage du gradient de couleur:
+        //*** VOTRE CODE
+        HashMap<Etat, Double> previousV = (HashMap<Etat, Double>) this.map.clone();
+
+        for (Etat e : this.mdp.getEtatsAccessibles()) {
+            List<Action> l = mdp.getActionsPossibles(e);
+            Double max_value = null;
+            for (Action a : l) {
+                try {
+                    Map<Etat, Double> possibilites = mdp.getEtatTransitionProba(e, a);
+                    Double value = 0.0;
+                    for (Map.Entry<Etat, Double> entry : possibilites.entrySet()) {
+                        if (possibilites == null || entry.getValue().compareTo(possibilites.getValue()) > 0) {
+                            possibilites = entry;
+                        }
+                    }
+                } catch (Exception ex) {
+                }
+            }
+        }
+        // mise a jour vmax et vmin pour affichage du gradient de couleur:
         //vmax est la valeur de max pour tout s de V
         //vmin est la valeur de min pour tout s de V
         // ...
@@ -68,19 +90,19 @@ public class ValueIterationAgent extends PlanningValueAgent {
      */
     @Override
     public Action getAction(Etat e) {
-		//*** VOTRE CODE
+        //*** VOTRE CODE
         List<Action> liste = this.getPolitique(e);
-        if(liste.size()>0)
-        return liste.get(new Random().nextInt(liste.size()));
+        if (liste.size() > 0) {
+            return liste.get(new Random().nextInt(liste.size()));
+        }
         return null;
 
     }
 
     @Override
     public double getValeur(Etat _e) {
-		//*** VOTRE CODE
-
-        return 0.0;
+        //*** VOTRE CODE
+        return this.map.get(_e);
     }
 
     /**
@@ -90,17 +112,38 @@ public class ValueIterationAgent extends PlanningValueAgent {
      */
     @Override
     public List<Action> getPolitique(Etat _e) {
-        List<Action> l = new ArrayList<Action>();
-		//*** VOTRE CODE
+        List<Action> l = mdp.getActionsPossibles(_e);
+        List<Action> r = new ArrayList<>();
+        Double max_value = null;
 
-        return l;
+        for (Action a : l) {
+
+            try {
+                Map<Etat, Double> possibilites = mdp.getEtatTransitionProba(_e, a);
+                Double value = 0.0;
+                for (Map.Entry<Etat, Double> entry : possibilites.entrySet()) {
+                    value += entry.getValue();
+                }
+                if (max_value == null || value > max_value) {
+                    r.clear();
+                    r.add(a);
+                    max_value = value;
+                } else if (Objects.equals(max_value, value)) {
+                    r.add(a);
+                }
+            } catch (Exception ex) {
+            }
+
+        }
+
+        return r;
 
     }
 
     @Override
     public void reset() {
         super.reset();
-		//*** VOTRE CODE
+        //*** VOTRE CODE
 
         /*-----------------*/
         this.notifyObs();
